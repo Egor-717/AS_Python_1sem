1efdd7de9c9ac3b44ae460fd1a0ef20fa0ed3c5c
Egor-717 <agapov2703egor@gmail.com>
2025-12-23 16:49:57 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
     pass
+
+
+import math
+
+def DegToRad(D):
+    if D <= 0 or D >= 360:
+        raise ValueError("Угол должен быть в диапазоне: 0 < D < 360")
+
+    radians = D * math.pi / 180
+    return radians
