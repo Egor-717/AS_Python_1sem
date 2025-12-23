f0e4e0bcc29b7ca4d42a8ecc9ee54cf9f162728a
Egor-717 <agapov2703egor@gmail.com>
2025-12-23 16:36:00 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,39 @@
 if __name__ == "__main__":
     pass
+
+
+
+
+
+python
+print("1. Проверка степени двойки (исключения)")
+print("2. Проверка палиндрома (исключения)")
+print("3. Проверка степени двойки (assert)")
+
+choice = input("Выберите задачу (1-3): ")
+
+if choice == '1':
+    num = int(input("Введите число: "))
+    if num <= 0:
+        raise ValueError("Число должно быть положительным")
+    if (num & (num - 1)) == 0:
+        print("Число является степенью двойки")
+    else:
+        raise Exception("Число не является степенью двойки")
+
+elif choice == '2':
+    text = input("Введите строку: ")
+    cleaned_text = ''.join(text.lower().split())
+    if cleaned_text == cleaned_text[::-1]:
+        print("Строка является палиндромом")
+    else:
+        raise Exception("Строка не является палиндромом")
+
+elif choice == '3':
+    num = int(input("Введите число: "))
+    assert num > 0, "Число должно быть положительным"
+    assert (num & (num - 1)) == 0, "Число не является степенью двойки"
+    print("Число является степенью двойки")
+
+else:
+    print("Неверный выбор")
