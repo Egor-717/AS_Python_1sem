67fd867f3a86f105450cffca7817653c927a0ca3
Egor-717 <agapov2703egor@gmail.com>
2025-12-23 16:55:23 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,25 @@
 if __name__ == "__main__":
     pass
+
+
+
+def Bell(L):
+    if not L:
+        return []
+
+    sorted_L = sorted(L)
+
+    result = []
+    left, right = 0, len(sorted_L) - 1
+    from_left = True  
+
+    while left <= right:
+        if from_left:
+            result.append(sorted_L[left])
+            left += 1
+        else:
+            result.append(sorted_L[right])
+            right -= 1
+        from_left = not from_left
+
+    return result
